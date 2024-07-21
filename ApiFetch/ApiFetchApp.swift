4cd5955c3f05f9a5aad2deab49c986028806686e
Chris Gonzalez <christogonz92@gmail.com>
2024-07-21 10:22:08 +0200

    Initial Commit

---
+++ ApiFetch/ApiFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiFetchApp.swift
+//  ApiFetch
+//
+//  Created by Christopher Gonzalez on 2024-07-21.
+//
+
+import SwiftUI
+
+@main
+struct ApiFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiFetch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApiFetch
+//
+//  Created by Christopher Gonzalez on 2024-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
